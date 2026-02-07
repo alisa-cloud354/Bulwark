d9c6cff42c16350c217272adb38832c6ba36a133
Vadzim Simanau <eks220885@gmail.com>
2026-02-07 19:44:24 +0200

    mode viteconfig and the ways in header and mobile menu

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./",
   plugins: [injectHTML()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        history: resolve(__dirname, "src/pages/history.html"),
+        zarok: resolve(__dirname, "src/pages/zarok-vajara.html"),
+        // Дадай астатнія (projects, reports і г.д.), калі яны гатовыя
+      },
+    },
+  },
 });

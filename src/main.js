75aa7ff21320700a8efc6606e6013cf1c2b21ff9
Vadzim Simanau <eks220885@gmail.com>
2026-03-03 13:51:18 +0200

    perf: conditional module init, fix navigation double call

--- src/main.js
+++ src/main.js
@@ -1,35 +1,37 @@
 import "./css/main.css";
 import { initHeader } from "./js/header.js";
 import { initLanguageSwitcher } from "./js/i18n.js";
-
-import { initNewsSlider } from "./js/news.js";
-import { initFullReportsGrid } from "./js/reports.js";
 import { initNavigation } from "./js/navigation.js";
+import { initModalControl } from "./js/modal.js";
+import { initCookies } from "./js/cookies.js";
+import { initNewsSlider } from "./js/news.js";
+import { initFullNewsGrid } from "./js/news-full.js";
+import { initFullReportsGrid } from "./js/reports.js";
 import { initContacts } from "./js/contacts.js";
 import { initZarokVajara } from "./js/zarok-vajara.js";
-import { initFullNewsGrid } from "./js/news-full.js";
 import { initGallerySlider } from "./js/gallery.js";
 import { initMaterials } from "./js/materials.js";
-import { initModalControl } from "./js/modal.js";
 import { initDonationSection } from "./js/donation-section.js";
 import { initForms } from "./js/forms.js";
-import { initCookies } from "./js/cookies.js";
 import { initPartners } from "./js/partners.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Заўсёды на ўсіх старонках
   initHeader();
   initLanguageSwitcher();
-  initNewsSlider();
-  initFullReportsGrid();
   initNavigation();
-  initContacts();
-  initZarokVajara();
-  initFullNewsGrid();
-  initGallerySlider();
-  initMaterials();
   initModalControl();
-  initDonationSection();
-  initForms();
   initCookies();
-  initPartners();
+
+  // Толькі калі элемент ёсць на старонцы
+  if (document.querySelector(".news-slider")) initNewsSlider();
+  if (document.querySelector("#news-grid-full")) initFullNewsGrid();
+  if (document.querySelector("#reports-grid-full")) initFullReportsGrid();
+  if (document.querySelector(".contact-sidebar")) initContacts();
+  if (document.querySelector(".animate-scroll-vertical")) initZarokVajara();
+  if (document.querySelector("#gallery-container")) initGallerySlider();
+  if (document.querySelector("#materials-container")) initMaterials();
+  if (document.querySelector("#donate")) initDonationSection();
+  if (document.querySelector("#help-form, #partners-form")) initForms();
+  if (document.querySelector("#partners-list")) initPartners();
 });

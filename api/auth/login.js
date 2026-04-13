2053f4cc85fe0f69f2d1d02022f8bfc74b9921b6
Vadzim Simanau <eks220885@gmail.com>
2026-04-13 22:27:50 +0300

    add change for rede[loy

--- api/auth/login.js
+++ api/auth/login.js
@@ -19,3 +19,4 @@ export default async function handler(req, res) {
   const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: "8h" });
   return res.status(200).json({ success: true, token });
 }
+//камент каб зрабіць рэдэплойй
